27c59b2e583701bb1e3730ae84dfc321f1060fdf
jo2312al <sharkdark2312@gmail.com>
2025-03-18 21:02:55 -0600

    comi

--- src/app/actividad/actividad.page.ts
+++ src/app/actividad/actividad.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ModalController, AlertController, InfiniteScrollCustomEvent } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-actividad',
@@ -8,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActividadPage implements OnInit {
 
-  constructor() { }
+  evaluacions: any = [];
+  public baseUrl: string = "http://localhost:8080/evaluacions";
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
+    this.cargarevaluacions();
   }
+ 
+  async cargarevaluacions(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando',
+      spinner: 'bubbles',
+    });
 
+    await loading.present();
+
+    try {
+      const response = await axios.get(this.baseUrl, {
+        withCredentials: true,
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+
+      this.evaluacions = response.data;
+    } catch (error) {
+      console.error('Error al cargar evaluacions:', error);
+    } finally {
+      loading.dismiss();
+      event?.target?.complete(); // Asegura que el infinite scroll se complete
+    }
+  }
 }
